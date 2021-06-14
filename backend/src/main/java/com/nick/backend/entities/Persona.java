d307c6098093db647e8dbafc42df9a594a59311b
Nick Zárate <nickzaro@gmail.com>
2021-06-14 11:20:16 -0300

    Cambiado de data.sql a import.sql para agregar datos por parte de spring

--- backend/src/main/java/com/nick/backend/entities/Persona.java
+++ backend/src/main/java/com/nick/backend/entities/Persona.java
@@ -1,15 +1,14 @@
 package com.nick.backend.entities;
 
-import jdk.internal.org.jline.utils.Colors;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-public class Persona {
+@Table(name = "personas")
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
